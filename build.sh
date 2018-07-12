5eea1338540a8799a6be5b58ee1dced66e4c1cd9
themrmax <max@fillr.com>
2018-07-12 17:28:21 +1000

    remove no-longer-supported --use-wheel flag

--- build.sh
+++ build.sh
@@ -16,10 +16,10 @@ yum install -y \
 
 do_pip () {
     pip install --upgrade pip wheel
-    test -f /outputs/requirements.txt && pip install --use-wheel -r /outputs/requirements.txt
-    pip install --use-wheel --no-binary numpy numpy
-    pip install --use-wheel --no-binary scipy scipy
-    pip install --use-wheel sklearn
+    test -f /outputs/requirements.txt && pip install -r /outputs/requirements.txt
+    pip install --no-binary numpy numpy
+    pip install --no-binary scipy scipy
+    pip install sklearn
 }
 
 strip_virtualenv () {
